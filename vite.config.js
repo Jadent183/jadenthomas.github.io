d3a9035b81a7271ce5f1d26f68782d29ef2db0ad
Jadent183 <Jadent183@gmail.com>
2024-11-02 14:50:22 -0700

    Fixed Deploy2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/jadenthomas.github.io/',
   assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.PNG'],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
